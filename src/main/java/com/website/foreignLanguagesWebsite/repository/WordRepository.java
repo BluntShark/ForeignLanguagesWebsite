f8ec737d38ce6b1e150d90aa10848d7622025f03
Anastasia <klansis3000@mail.ru>
2024-05-17 00:55:03 +1000

    исправлено set(list) с users и words

--- src/main/java/com/website/foreignLanguagesWebsite/repository/WordRepository.java
+++ src/main/java/com/website/foreignLanguagesWebsite/repository/WordRepository.java
@@ -3,12 +3,15 @@ package com.website.foreignLanguagesWebsite.repository;
 import com.website.foreignLanguagesWebsite.dto.UserDto;
 import com.website.foreignLanguagesWebsite.dto.WordDto;
 import com.website.foreignLanguagesWebsite.entity.wordentity.Word;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
-    //List<WordDto> findWordsByUserId(Long userId);
+//    @Transactional
+//    @Query("SELECT t FROM userword where t.user_id = ?1")
     List<WordDto> findByUsers(UserDto userDto);
 }
