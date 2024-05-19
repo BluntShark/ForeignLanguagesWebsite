4813724d4496d1f29de715637af64485740bc4e6
Anastasia <klansis3000@mail.ru>
2024-05-19 13:20:53 +1000

    security

--- src/main/java/com/website/foreignLanguagesWebsite/entity/userentity/Role.java
+++ src/main/java/com/website/foreignLanguagesWebsite/entity/userentity/Role.java
@@ -2,11 +2,13 @@ package com.website.foreignLanguagesWebsite.entity.userentity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Data
 @Table(name = "role")
 public class Role {
     @Id
